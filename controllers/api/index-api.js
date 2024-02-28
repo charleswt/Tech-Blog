81817efbd671966b63ab2a675a542fa7f2ccb3b5
ct <charleswtiffany@gmail.com>
2024-02-28 13:16:17 -0800

    added user-routes.js

--- controllers/api/index-api.js
+++ controllers/api/index-api.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const blogs = require('./blog-routes');
+const users = require('./user-routes')
 
-router.use('/blog', blogs)
+router.use('/blog', blogs);
+router.use('/user', users)
 
 module.exports = router;
---
+++ controllers/api/user-routes.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { User } = require('../../models/index-model');
+
+router.post('/login');
+
+module.exports = router;
