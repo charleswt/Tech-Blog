83cc411ebb456c02f23d4cf215c13bf4315f4d10
ct <charleswtiffany@gmail.com>
2024-03-07 22:02:07 -0800

    fixed minor error wasnt saving value to session

---
+++ controllers/api/comments-api.js
@@ -0,0 +1,6 @@
+const router = require('express').Router();
+const { Blog, Comments } = require('../../models/index-model')
+
+router.post('/comment', (req,res) => {})
+
+module.exports = router;
--- controllers/api/index-api.js
+++ controllers/api/index-api.js
@@ -1,8 +1,10 @@
 const router = require('express').Router();
 const blogs = require('./blog-routes');
 const users = require('./user-routes')
+const comments = require('./comments-api')
 
 router.use('/blog', blogs);
-router.use('/user', users)
+router.use('/user', users);
+router.use('/comments', comments)
 
 module.exports = router;
